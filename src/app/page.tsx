76f62baf592a06d1915c6fc702f5da3b14063e88
Yash More <yashmore2002bs@gmail.com>
2025-11-07 15:28:55 +0530

    testing sync between vercel and github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 }
