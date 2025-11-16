3214aa84cda586e07e250173b664be30abea69e3
Yash More <yashmore2002bs@gmail.com>
2025-11-16 18:56:55 +0530

    fix: github login flow

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,16 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google, GitHub],
-  pages: {
-    signIn: "/",
+  cookies: {
+    pkceCodeVerifier: {
+      name: "authjs.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: true,
+      },
+    },
   },
+  trustHost: true,
 });
